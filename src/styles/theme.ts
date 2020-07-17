4650caf860a2146283ab701734e27794792a8f6e
a-chris <a.christian.toscano@gmail.com>
2020-07-18 00:54:08 +0200

    set Lato as default font

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { theme } from '@chakra-ui/core';
+
+const customTheme = {
+    ...theme,
+    fonts: {
+        body: 'Lato',
+        heading: 'Lato',
+        mono: 'Lato',
+    },
+};
+export default customTheme;
